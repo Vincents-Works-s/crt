69ecad56c50d6a4dd8d69d6afe5514527fdf0b87
mission bit <missionbit@52a-MissionBit.hsd1.ca.comcast.net>
2020-10-07 18:37:46 -0700

    Done??

--- crt.py
+++ crt.py
@@ -1,12 +1,18 @@
 import random
-weapon = ["compost", "recycling", "trash"]
+weapon = ["compost", "recycle", "trash"]
+
+print("Welcome to Compost, Recycle, Trash")
+print()
+name = input("What is your name? ")
+
 you = 0
 computer = 0
 play_again = True
+
 while play_again == True:
     choice = input("Your choice: ")
     
-    while choice != "compost" and choice != "recycling" and choice != "trash":
+    while choice != "compost" and choice != "recycle" and choice != "trash":
         choice = input("Your choice: ")
         
     pc_move = random.randint(0, 2)
@@ -15,17 +21,17 @@ while play_again == True:
         if pc_move == 0:
             print(f"Opponent's move: compost   Tie")
         elif pc_move == 1:
-            print(f"Opponent's move: recycling   You win!")
+            print(f"Opponent's move: recycle   You win!")
             you += 1
         else:
             print(f"Opponent's move: trash   You lose..")
             computer += 1
-    if choice == "recycling":
+    if choice == "recycle":
         if pc_move == 0:
             print(f"Opponent's move: compost   You lose..")
             computer += 1
         elif pc_move == 1:
-            print(f"Opponent's move: recycling   Tie")
+            print(f"Opponent's move: recycle   Tie")
         else:
             print(f"Opponent's move: trash   You win!")
             you += 1
@@ -34,13 +40,22 @@ while play_again == True:
             print(f"Opponent's move: compost   You win!")
             you += 1
         elif pc_move == 1:
-            print(f"Opponent's move: recycling   You lose..")
+            print(f"Opponent's move: recycle   You lose..")
             computer += 1
         else:
             print(f"Opponent's move: trash   Tie")
-            
-    print(f"You: {you}   Computer: {computer}")
+        
+    print()
+    print(f"{name}: {you}   Computer: {computer}")
     print()
     response = input("Would you like to play again? ")
     if response == "no":
+        print()
+        total_games = you + computer
+        you_win_percent = (you / total_games) * 100
+        pc_win_percent = (computer / total_games) * 100
+        print(f"{name}'s winning percentage: {round(you_win_percent, 2)}%")
+        print(f"Computer's winning percentage: {round(pc_win_percent, 2)}%")
+        
+        
         play_again = False
